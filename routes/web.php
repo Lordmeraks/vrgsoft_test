d3d7d3e14309b0151416cbd3b451abdbc5353020
Lordmeraks <Lordmeraks@gmail.com>
2021-07-31 19:53:10 +0300

    make AuthorController.php and BookController.php make Route for them

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('authors', 'AuthorController');
+Route::resource('books', 'BookController');
